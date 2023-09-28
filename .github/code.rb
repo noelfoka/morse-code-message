1e0978d90fc7e1dbb9b05f48734914af6c060689
noelfoka <nomgnenoel@gmail.com>
2023-09-28 07:31:09 +0100

    update

---
+++ .github/code.rb
@@ -0,0 +1,39 @@
+def decode_char(code)
+  morse_code = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z"
+  }
+  morse_code[code].upcase
+end
+
+def decode_word(word)
+  word.split.map { |code| decode_char(code) }.join
+end
+
+def decode(message)
+  message.split("   ").map { |word| decode_word(word) }.join(" ")
+end
